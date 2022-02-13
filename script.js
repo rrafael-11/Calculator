c0a2cf7a9dee76a0207049f2171c178906de65cb
Rafael <rsc250505@gmail.com>
2022-02-13 17:41:00 -0300

    Add setDecimalPoint function and update operate function logic

--- script.js
+++ script.js
@@ -3,46 +3,34 @@ var numbers = document.querySelectorAll('.number')
 var operators = document.querySelectorAll('.operator')
 var equals = document.querySelector('.equals')
 var clearButton = document.querySelector('.clear')
+var decimalPoint = document.querySelector('.decimal-point')
 var opInput = ''
 var fullNumber = ''
 var num1 = ''
 var num2 = ''
 
-function clear() {
-    num1 = ''
-    num2 = ''
-    opInput = ''
-    fullNumber = ''
-    updateDisplay()
-}
-
-function restartValues() {
-    num2 = ''
-    opInput = ''
-    fullNumber = ''
+function updateDisplay() {
+    display.textContent = `${num1} ${opInput} ${num2}`
 }
 
 function operate() {
     if (num2 == false) {
         return null
-    } 
+    }
     switch (opInput) {
         case '+':
-            num1 = add(num1, num2);
+            fullNumber = add(num1, num2);
             break;
         case '-':
-            num1 = subtract(num1, num2);
+            fullNumber = subtract(num1, num2);
             break;
         case '*':
-            num1 = multiply(num1, num2);
+            fullNumber = multiply(num1, num2);
             break;
         case '/':
-            num1 = divide(num1, num2);
+            fullNumber = divide(num1, num2);
             break;
     }
-
-    restartValues()
-    updateDisplay()
 
     function add(num1, num2) {
         return parseInt(num1) + parseInt(num2)
@@ -59,6 +47,10 @@ function operate() {
     function divide(num1, num2) {
         return num1 / num2
     }
+
+    num1 = fullNumber
+    restartValues()
+    updateDisplay()
 }
 
 function setOperator(op) {
@@ -76,16 +68,31 @@ function setOperator(op) {
 }
 
 function setNumber(lastDigit) {
-    if (opInput) {
-        num2 = fullNumber += lastDigit
-    } else {
-        num1 = fullNumber += lastDigit
+    opInput ? num2 = fullNumber += lastDigit : num1 = fullNumber += lastDigit
+    updateDisplay()
+}
+
+function setDecimalPoint() {
+    if (display.textContent.includes('.')) {
+        return null
+    }
+    else {
+        opInput ? num2 = fullNumber += '.' : num1 = fullNumber += '.'
     }
     updateDisplay()
 }
 
-function updateDisplay() {
-    display.textContent = `${num1} ${opInput} ${num2}`
+function clear() {
+    num1 = ''
+    num2 = ''
+    opInput = ''
+    fullNumber = ''
+    updateDisplay()
+}
+
+function restartValues() {
+    num2 = ''
+    opInput = ''
 }
 
 for (var i = 0; i < numbers.length; i++) {
@@ -96,6 +103,8 @@ for (var i = 0; i < operators.length; i++) {
     operators[i].addEventListener('mousedown', (e) => setOperator(e.target.textContent))
 }
 
-equals.addEventListener('mousedown', () => {operate()})
+equals.addEventListener('mousedown', () => { operate() })
 
-clearButton.addEventListener('mousedown', () => {clear()})
+clearButton.addEventListener('mousedown', () => { clear() })
+
+decimalPoint.addEventListener('mousedown', () => { setDecimalPoint() })
