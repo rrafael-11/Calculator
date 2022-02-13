971341c0155158d9cc677abf22104f493db429cf
Rafael <rsc250505@gmail.com>
2022-02-13 16:01:22 -0300

    Add functions to set number, operator and update the display

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var display = document.querySelector('.display')
+var numbers = document.querySelectorAll('.number')
+var operators = document.querySelectorAll('.operator')
+var opInput = ''
+var fullNumber = ''
+var num1 = ''
+var num2 = ''
+
 function operate(op, num1, num2) {
     function add(num1, num2) {
         return num1 + num2
@@ -14,4 +22,35 @@ function operate(op, num1, num2) {
     function divide(num1, num2) {
         return num1 / num2
     }
+}
+
+function setOperator(op) {
+    if (num1 == false) {
+        return null
+    } else {
+        fullNumber = ''
+        opInput = op
+        updateDisplay()
+    }
+}
+
+function setNumber(lastDigit) {
+    if (opInput) {
+        num2 = fullNumber += lastDigit
+    } else {
+        num1 = fullNumber += lastDigit
+    }
+    updateDisplay()
+}
+
+function updateDisplay() {
+    display.textContent = `${num1} ${opInput} ${num2}`
+}
+
+for (var i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('mousedown', (e) => setNumber(e.target.getAttribute('data-number')))
+}
+
+for (var i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('mousedown', (e) => setOperator(e.target.textContent))
 }
