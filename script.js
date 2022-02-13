e22292bf7297e6de1e03fbd77a186ceb112d7db4
Rafael <rsc250505@gmail.com>
2022-02-13 20:33:57 -0300

    add hoverButton function and clean up the code

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var equals = document.querySelector('.equals')
 var clearButton = document.querySelector('.clear')
 var decimalPoint = document.querySelector('.decimal-point')
 var opInput = ''
-var fullNumber = ''
 var num1 = ''
 var num2 = ''
 
@@ -14,33 +13,31 @@ function updateDisplay() {
 }
 
 function operate() {
-    if (num2 == false) {
+    if (num2 == '') {
         return null
     }
     switch (opInput) {
         case '+':
-            fullNumber = add(num1, num2);
+            num1 = add(num1, num2);
             break;
         case '-':
-            fullNumber = subtract(num1, num2);
+            num1 = subtract(num1, num2);
             break;
         case '*':
-            fullNumber = multiply(num1, num2);
+            num1 = multiply(num1, num2);
             break;
         case '/':
             if (num2 == '0') {
-                alert(`You can't divide by zero!`)
-                num2 = ''
+                return alert(`You can't divide by zero!`)
             } else {
-                fullNumber = divide(num1, num2);
+                num1 = divide(num1, num2);
             }
             break;
         case '÷':
             if (num2 == '0') {
-                alert(`You can't divide by zero!`)
-                num2 = ''
+                return alert(`You can't divide by zero!`)
             } else {
-                fullNumber = divide(num1, num2);
+                num1 = divide(num1, num2);
             }
             break;
     }
@@ -61,7 +58,11 @@ function operate() {
         return num1 / num2
     }
 
-    num1 = fullNumber
+    function restartValues() {
+        num2 = ''
+        opInput = ''
+    }
+
     restartValues()
     updateDisplay()
 }
@@ -74,14 +75,13 @@ function setOperator(op) {
         opInput = op
         updateDisplay()
     } else {
-        fullNumber = ''
         opInput = op
         updateDisplay()
     }
 }
 
 function setNumber(lastDigit) {
-    opInput ? num2 = fullNumber += lastDigit : num1 = fullNumber += lastDigit
+    opInput ? num2 = num2 += lastDigit : num1 = num1 += lastDigit
     updateDisplay()
 }
 
@@ -90,22 +90,31 @@ function setDecimalPoint() {
         return null
     }
     else {
-        opInput ? num2 = fullNumber += '.' : num1 = fullNumber += '.'
+        if (opInput) {
+            num2 = num2 += '.'
+        } else {
+            num1 = num1 += '.'
+        }
     }
     updateDisplay()
 }
 
+function hoverButton(id, type) {
+    document.querySelector('#' + id).classList.toggle(`${type}-active`)
+    setTimeout(() => {
+        document.querySelector('#' + id).classList.toggle(`${type}-active`)
+    }, 250)
+}
+
 function backspace() {
     if (opInput && num2) {
-        fullNumber = fullNumber.toString().slice(0, fullNumber.toString().length - 1)
-        num2 = fullNumber
+        num2 = num2 = num2.toString().slice(0, num2.toString().length - 1)
     }
     else if (opInput && num2 == false) {
         opInput = ''
     }
     else {
-        fullNumber = fullNumber.toString().slice(0, fullNumber.toString().length - 1)
-        num1 = fullNumber
+        num1 = num1.toString().slice(0, num1.toString().length - 1)
     }
 
     updateDisplay()
@@ -115,13 +124,7 @@ function clear() {
     num1 = ''
     num2 = ''
     opInput = ''
-    fullNumber = ''
     updateDisplay()
-}
-
-function restartValues() {
-    num2 = ''
-    opInput = ''
 }
 
 for (var i = 0; i < numbers.length; i++) {
@@ -129,7 +132,9 @@ for (var i = 0; i < numbers.length; i++) {
 }
 
 for (var i = 0; i < operators.length; i++) {
-    operators[i].addEventListener('mousedown', (e) => setOperator(e.target.textContent))
+    operators[i].addEventListener('mousedown', (e) => {
+        setOperator(e.target.textContent)
+    })
 }
 
 equals.addEventListener('mousedown', () => { operate() })
@@ -141,58 +146,77 @@ decimalPoint.addEventListener('mousedown', () => { setDecimalPoint() })
 document.addEventListener('keydown', (e) => {
     switch (e.key) {
         case '1':
+            hoverButton('num1', 'number')
             setNumber('1')
             break;
         case '2':
+            hoverButton('num2', 'number')
             setNumber('2')
             break;
         case '3':
+            hoverButton('num3', 'number')
             setNumber('3')
             break;
         case '4':
+            hoverButton('num4', 'number')
             setNumber('4')
             break;
         case '5':
+            hoverButton('num5', 'number')
             setNumber('5')
             break;
         case '6':
+            hoverButton('num6', 'number')
             setNumber('6')
             break;
         case '7':
+            hoverButton('num7', 'number')
             setNumber('7')
             break;
         case '8':
+            hoverButton('num8', 'number')
             setNumber('8')
             break;
         case '9':
+            hoverButton('num9', 'number')
             setNumber('9')
             break;
         case '0':
+            hoverButton('num0', 'number')
             setNumber('0')
             break;
         case '+':
+            hoverButton('add', 'operator')
             setOperator('+')
             break;
         case '-':
+            hoverButton('subtract', 'operator')
             setOperator('-')
             break;
         case '*':
+            hoverButton('multiply', 'operator')
             setOperator('*')
             break;
         case '/':
+            hoverButton('divide', 'operator')
             setOperator('/')
+            break;
+        case '.':
+            hoverButton('point', 'decimal-point')
+            setDecimalPoint()
+            break;
+        case '=':
+            hoverButton('equals', 'equals')
+            operate()
             break;
         case 'Backspace':
             backspace()
             break;
-        case '.':
-            setDecimalPoint()
-            break;
-        case '=':
-            operate()
-            break;
         case 'Enter':
             operate()
+            break;
+        case 'c':
+            clear()
             break;
     }
 })
